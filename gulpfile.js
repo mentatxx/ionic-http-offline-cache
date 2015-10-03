e2a3d470591e7ccfe152e5d9b8a9a60936eec8f8
Alexey Petushkov <mentatxx@gmail.com>
2015-10-03 15:59:03 +0300

    Added sourcemap file generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   return gulp.src(['./src/HttpOfflineCache.js', './src/HttpOfflineCacheStorage.js'])
+      .pipe(sourcemaps.init())
       .pipe(concat('HttpOfflineCache.min.js'))
       .pipe(uglify())
+      .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../js'}))
       .pipe(gulp.dest('./dist/'));
 });
